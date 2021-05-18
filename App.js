71443f6ecadfad490502454e6e4c91c00a47153c
Ivan Polyakov <ivan@inbox.ru>
2021-05-18 19:36:01 +0400

    The first commit

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { HomeScreen } from './screens/HomeScreen';
+import { CategoriesScreen } from './screens/CategoriesScreen';
 
-export default function App() {
+function DetailsScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+        <Button
+            title="Go to Details... again"
+            onPress={() => navigation.push('Details')}
+        />
+        <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+        <Button title="Go back" onPress={() => navigation.goBack()} />
+        <Button
+            title="Go back to first screen in stack"
+            onPress={() => navigation.popToTop()}
+        />
+      </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen}  />
+          <Stack.Screen name="Categories" component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import { Button, View, Text } from 'react-native';
+
+export class CategoriesScreen extends Component {
+    render = () => {
+        return (
+            <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+                <Text>Категории карточек</Text>
+                <Button title="Главный экран" onPress={() => this.props.navigation.navigate('Home')}/>
+                <Button title="Назад" onPress={() => this.props.navigation.goBack()}/>
+                <Button
+                    title="В начало приложения"
+                    onPress={() => this.props.navigation.popToTop()}
+                />
+            </View>
+        );
+    }
+}
